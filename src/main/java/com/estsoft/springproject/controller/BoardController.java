ee2756bc31364ee85df470f9ca356780ffaa9559
inwoo <jiw413@naver.com>
2024-03-25 16:05:18 +0900

    Fix: BoardController url 수정

--- src/main/java/com/estsoft/springproject/controller/BoardController.java
+++ src/main/java/com/estsoft/springproject/controller/BoardController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/boards")
+@RequestMapping("/boards")
 @Slf4j
 public class BoardController {
     private final BoardService boardService;
