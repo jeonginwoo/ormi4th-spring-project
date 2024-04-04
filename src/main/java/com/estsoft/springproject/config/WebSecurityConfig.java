b15e5d7e18602b15ffc3310c162ecdf6d7effc28
euichan0927 <yuichan99@naver.com>
2024-04-04 16:12:05 +0900

    feat : 회원가입 닉네임 중복 검사 추가 및 마이페이지 게시글 및 댓글 페이징 처리

--- src/main/java/com/estsoft/springproject/config/WebSecurityConfig.java
+++ src/main/java/com/estsoft/springproject/config/WebSecurityConfig.java
@@ -19,10 +19,11 @@ public class WebSecurityConfig {
 	}
 
 	// 특정 HTTP 요청에 대한 웹 기반 보안 구성
+	// 특정 HTTP 요청에 대한 웹 기반 보안 구성
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeHttpRequests(auth ->              // 인증, 인가 설정
-				auth.requestMatchers("/login", "/signup", "/user").permitAll()
+				auth.requestMatchers("/login", "/signup").permitAll()
 					.anyRequest().authenticated())
 			.formLogin(auth -> auth.loginPage("/login")     // 폼 기반 로그인 설정
 				.defaultSuccessUrl("/"))
