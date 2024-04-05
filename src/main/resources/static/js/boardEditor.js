8503d6c853f4cfae4669582c115af896028fb7a8
inwoo <jiw413@naver.com>
2024-04-05 14:45:14 +0900

    Feat: toast ui 이미지 링크처리

--- src/main/resources/static/js/boardEditor.js
+++ src/main/resources/static/js/boardEditor.js
@@ -4,7 +4,36 @@ const editor = new toastui.Editor({
     previewStyle: 'vertical',
     height: '500px',
     initialValue: boardContent,
-    placeholder: '내용 입력'
+    placeholder: '내용 입력',
+    hooks: {
+        async addImageBlobHook(blob, callback) { // 이미지 업로드 로직 커스텀
+            try {
+                /*
+                 * 1. 에디터에 업로드한 이미지를 FormData 객체에 저장
+                 *    (이때, 컨트롤러 uploadEditorImage 메서드의 파라미터인 'image'와 formData에 append 하는 key('image')값은 동일해야 함)
+                 */
+                const formData = new FormData();
+                formData.append('image', blob);
+
+                // 2. FileApiController - uploadEditorImage 메서드 호출
+                const response = await fetch('/tui-editor/image-upload', {
+                    method : 'POST',
+                    body : formData,
+                });
+
+                // 3. 컨트롤러에서 전달받은 디스크에 저장된 파일명
+                const filename = await response.text();
+                console.log('서버에 저장된 파일명 : ', filename);
+
+                // 4. addImageBlobHook의 callback 함수를 통해, 디스크에 저장된 이미지를 에디터에 렌더링
+                const imageUrl = `/tui-editor/image-print?filename=${filename}`;
+                callback(imageUrl, 'image alt attribute');
+
+            } catch (error) {
+                console.error('업로드 실패 : ', error);
+            }
+        }
+    }
 });
 
 // 게시글 수정
