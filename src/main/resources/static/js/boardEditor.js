ba58835f7b331a59cbd6d11a406ab96efd5d08d1
inwoo <jiw413@naver.com>
2024-04-05 15:54:25 +0900

    Feat: 글쓰기, 수정에서 제목, 내용 글자수 제한 추가

--- src/main/resources/static/js/boardEditor.js
+++ src/main/resources/static/js/boardEditor.js
@@ -6,35 +6,44 @@ const editor = new toastui.Editor({
     initialValue: boardContent,
     placeholder: '내용 입력',
     hooks: {
-        async addImageBlobHook(blob, callback) { // 이미지 업로드 로직 커스텀
+        async addImageBlobHook(blob, callback) {
             try {
-                /*
-                 * 1. 에디터에 업로드한 이미지를 FormData 객체에 저장
-                 *    (이때, 컨트롤러 uploadEditorImage 메서드의 파라미터인 'image'와 formData에 append 하는 key('image')값은 동일해야 함)
-                 */
                 const formData = new FormData();
                 formData.append('image', blob);
 
-                // 2. FileApiController - uploadEditorImage 메서드 호출
                 const response = await fetch('/tui-editor/image-upload', {
-                    method : 'POST',
-                    body : formData,
+                    method: 'POST',
+                    body: formData,
                 });
 
-                // 3. 컨트롤러에서 전달받은 디스크에 저장된 파일명
                 const filename = await response.text();
                 console.log('서버에 저장된 파일명 : ', filename);
 
-                // 4. addImageBlobHook의 callback 함수를 통해, 디스크에 저장된 이미지를 에디터에 렌더링
                 const imageUrl = `/tui-editor/image-print?filename=${filename}`;
                 callback(imageUrl, 'image alt attribute');
 
             } catch (error) {
                 console.error('업로드 실패 : ', error);
             }
+        },
+        change: function() {
+            const content = editor.getMarkdown(); // 에디터의 Markdown 내용 가져오기
+            const contentLength = content.length;
+            const maxLength = 3000; // 최대 글자 수 설정
+
+            // 에디터 하단에 글자 수 표시
+            const characterCountElement = document.querySelector('#character-count');
+            characterCountElement.textContent = `${contentLength} / ${maxLength}`;
+
+            if (contentLength > maxLength) { // 3000자를 초과하는 경우
+                const trimmedContent = content.slice(0, maxLength); // 초과된 부분 자르기
+                editor.setMarkdown(trimmedContent); // 에디터 내용 설정
+                alert("3000자 이하로 작성해주세요.");
+            }
         }
     }
 });
+
 
 // 게시글 수정
 function updateBoard() {
