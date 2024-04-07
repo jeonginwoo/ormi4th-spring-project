baa189e336db3c00ee0a5051ac4193a38c7a21e6
fernandokkang <spineattack@gmail.com>
2024-04-08 03:49:56 +0900

    appspec 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/base-loaded-web-app.jar"
+JAR_FILE="$PROJECT_ROOT/spring-project.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
